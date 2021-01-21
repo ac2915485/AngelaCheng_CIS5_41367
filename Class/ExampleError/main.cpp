8d8be5c7c427df9c367ff11c38220ad8a6d9be0f
Angela Cheng <angela@MacBook-Pro.local>
2021-01-20 22:34:30 -0800

    Update

---
+++ Class/ExampleError/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: angela
+ *
+ * Created on January 4, 2021, 8:51 PM
+ * Purpose: To show computers can't add/subtract
+ */
+
+/*
+ *This is a block comment
+ */
+
+//System Libraries
+#include <iostream> //I/O Library
+using namespace std;
+
+//User Libraries
+
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    short a,b,c;
+    
+    //Initialize Variables
+    a=30000;
+    b=4000;
+    
+    //Map Inputs to Outputs -> Process
+    c=a+b;
+    
+    //Display Inputs/Outputs
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
+
